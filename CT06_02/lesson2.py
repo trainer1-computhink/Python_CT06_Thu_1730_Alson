7a705e2903428e0aad145875b8b6093adfc03ea5
trainer1-computhink <trainer1@computhink.com.sg>
2026-01-08 11:00:40 +0000

    Lesson 1

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -1,7 +1,43 @@
-print("Hello from lesson 2")
+# Recap 1
+# print("Red")
+# print("Orange")
+print("Yellow")
+# print("Green")
+# print("Blue")
+# print("Indigo")
+# print("Violet")
 
-######## Write the pseudocode in comments for task 2 here
-# Using comments, translate the code shown on screen into pseudocode.
+# Task 1a
+# Say Hey! and move 10 steps, 10 times.
 
-######## Write the pseudocode in comments for task 3 here
-# Using comments, translate the code shown on screen into pseudocode.
+# Task 1b
+# Counter starts at 0.
+# Move 10 steps, turn 15 degrees and increase counter by 10.
+# Repeat until counter reaches 50.
+
+# Task 1c
+# Ask user for their age.
+# Store their age as answer.
+# Check if answer is less than 18.
+# If it is less than 18, say Access Denied!
+# Else say Welcome!
+
+# Task 2
+# Make and label Glass, Plastic and Paper bins.
+# Check the material of the item received.
+# If it is glass, put it in the Glass bin.
+# else if it is plastic, put it in the Plass bin.
+# else if it is paper, put it in the Plass bin.
+
+# Task 3
+# Ask user for password.
+# Check if user password = secret phrase.
+# If it is correct, say Congratulations.
+# If it is wrong, say Error
+
+# Task 4
+# Test 1 = Test 1 x 0.20
+# Test 2 = Test 2 x 0.40
+# Test 3 = Test 3 x 0.40
+
+# Student's final score = Test 1 + Test 2 + Test 3
