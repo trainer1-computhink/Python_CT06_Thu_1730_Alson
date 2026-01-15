3134ee3c6620796de9b5c705bcdc8a2dfa56fd75
trainer1-computhink <trainer1@computhink.com.sg>
2026-01-15 11:05:31 +0000

    Lesson 2

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,82 @@
-print("Hello from lesson 3")
+import math
+
+# my_favourite_food = "Banana"
+# print(my_favourite_food)
+# my_number = 100
+# print(my_number)
+
+# Task 1a
+# age = 100
+# print(age)
+# print("age")
+
+# Task 1b
+# x = 10
+# x = 20
+# print(x)
+
+# Task 2a
+# variable1 = 3
+# variable2 = 5
+# print(variable1 + variable2)
+
+# Task 2b
+# print(variable1 * variable2)
+# print(variable1 / variable2)
+# print(variable1 - variable2)
+
+# Task 3a
+# num1 = 42.56789
+# print(round(num1, 2))
+
+# Task 3b
+# num2 = 1267.15
+# print(round(num2))
+
+# Task 3c
+# num3 = 1785.956
+# print(round(num3, -2))
+
+# Task 4a
+# a = 10
+# b = 20
+# ans = a + b
+# print(ans)
+
+# Task 4b
+# num1 = 10
+# num2 = 50
+# ans = (num2 - num1) * 3
+# print(ans)
+
+# Task 4c
+# num1 = 100
+# num2 = 4
+# ans = num1 / num2
+# print(ans)
+
+# Task 4d
+# ans = (3 + 4) * 5 - (10 / 2)
+# print(ans)
+
+# Task 5a
+# pineapple_tarts = 15 * 2
+# bak_kwa = 58
+# total = pineapple_tarts + bak_kwa
+# print(round(total, 2))
+
+# Task 5b
+# seafood_platter = 96
+# side_dishes = 18 * 3
+# rice = 2 * 6
+# total = (seafood_platter + side_dishes + rice) * 1.1 - 25
+# print(round(total, 2))
+
+# Task 6a
+distance = abs(-55)
+print(distance)
+
+# Task 6b
+degrees = abs(-8)
+print(degrees)
+
