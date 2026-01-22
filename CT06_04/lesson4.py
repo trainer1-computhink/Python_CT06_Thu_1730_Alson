a49352058b75b1717a6486827912c7de6eda23d1
trainer1-computhink <trainer1@computhink.com.sg>
2026-01-22 11:00:24 +0000

    2026-01-22

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,1 +1,29 @@
-print("Hello from lesson 4")
+# print("Hello from lesson 4")
+
+# # input()
+# name = input("What is your name: ")
+# food = input("What food do you want to eat: ")
+
+# sentence = name + " likes to eat " + food # string concatenation
+# print(sentence)
+
+# ## Challenge 1: ask for hobby, then print {name} likes to {hobby}
+# ## Challenge 2: ask for which is favourite country to go holiday, then print, {name} would like to go to {place}
+
+#########################################
+# ask for 2 numbers, and add it together
+
+num1 = input("What is the first number? ") # string
+num2 = input("What is the second number? ") # string
+num3 = int(num1) + int(num2)                         #type conversion>>> convert from something to integer    int()   str()
+
+# string formatting. f-string
+
+sentence = f"{num1} + {num2} = {num3}" # format a string
+print(sentence)
+
+
+
+# print(num1 + " + " + num2 + " = " + str(num3))
+
+####
