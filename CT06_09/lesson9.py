955e87e75140f6e723747f4005ae234b6eaf3c3c
trainer1-computhink <trainer1@computhink.com.sg>
2026-03-19 11:06:36 +0000

    Lesson 9

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,1 +1,31 @@
-print("Hello from lesson 9")
+# Recap 1
+# import random
+
+# num1 = random.randint(1,6)
+# num2 = random.randint(1,6)
+# num3 = random.randint(1,6)
+
+# print(f"1st number: {num1}")
+# print(f"2nd number: {num2}")
+# print(f"3rd number: {num3}")
+
+# all_even_odd = False
+
+# if num1 % 2 == 0 and num2 % 2 == 0 and num3 % 2 == 0:
+#     all_even_odd = True
+# elif num1 % 2 == 1 and num2 % 2 == 1 and num3 % 2 == 1:
+#     all_even_odd = True
+
+# print(f"All numbers are even/odd: {all_even_odd}")
+
+# Task 2
+# num_days = int(input("How many days have you borrowed the book?\n"))
+
+# if num_days > 25:
+#     print("Remember to return your book!")
+
+# Task 3b
+num_apples = input("How many apples% do you want to buy?\n")
+
+if num_apples > 10:
+    print()
