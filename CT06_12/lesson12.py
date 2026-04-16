31317d2da07eeaa74ae2d9a5c666ef5da5bf9ce9
trainer1-computhink <trainer1@computhink.com.sg>
2026-04-16 11:00:41 +0000

    Lesson 13

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -46,33 +46,50 @@
 #     print("Happy New Year!")
 
 # Task 5
+# import random
+
+# score = 0
+# correct_answers = 0
+# skip = 0
+# while True:
+#     num1 = random.randint(1, 20)
+#     num2 = random.randint(1, 20)
+#     operation = random.randint(1, 3)
+#     if operation == 1:
+#         ans = num1 + num2
+#         operation_sign = "+"
+#     elif operation == 2:
+#         ans = num1 - num2
+#         operation_sign = "-"
+#     else:
+#         ans = num1 * num2
+#         operation_sign = "x"
+
+#     user_ans = input(f"What is {num1} {operation_sign} {num2}?\n")
+#     if user_ans.lower() == "skip":
+#         skip += 1
+#         print(f"You have skipped {skip} question(s).")
+#         if skip == 5:
+#             print(f"You have skipped too many questions.\nYou are disqualified.")
+#             break
+#     elif int(user_ans) == ans:
+#         print(f"That's correct!")
+#         correct_answers += 1
+#         score += 2
+#         if correct_answers == 5:
+#             print(f"You scored: {score}")
+#             break
+#     else:
+#         print("That's wrong! Try again.")
+#         score -= 1
+#         if score <= 0:
+#             print("You have answered wrongly too many times.\nYou are disqualified.")
+#             break
+
+# Task 6
 import random
 
-score = 0
-correct_answers = 0
-while True:
-    num1 = random.randint(1, 20)
-    num2 = random.randint(1, 20)
-    operation = random.randint(1, 3)
-    if operation == 1:
-        ans = num1 + num2
-        operation_sign = "+"
-    elif operation == 2:
-        ans = num1 - num2
-        operation_sign = "-"
-    else:
-        ans = num1 * num2
-        operation_sign = "x"
-
-    user_ans = input(f"What is {num1} {operation_sign} {num2}?\n")
-
-    if int(user_ans) == ans:
-        print(f"That's correct!")
-        correct_answers += 1
-        score += 2
-        if correct_answers == 5:
-            print(f"You scored: {score}")
-            break
-    else:
-        print("That's wrong! Try again.")
-        score -= 1
+num = 0
+while num != 4:
+    num = random.randint(1, 6)
+    print(f"You rolled a {num}.")
--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,1 +1,73 @@
-print("Hello from lesson 13")
+# Recap 1
+# balance = 1000
+# while True:
+#     print("--- Welcome to the ATM ---\n")
+#     print(f"Current balance : ${balance}\n")
+#     print("1. Withdraw")
+#     print("2. Deposit")
+#     print("3. Check Balance")
+#     print("4. Exit\n")
+#     choice = input("Enter your choice: ")
+
+#     if choice == "1":
+#         withdraw_amount = input("Enter the amount you want to withdraw: ")
+
+#         if not withdraw_amount.isdigit():     
+#             print("\nInvalid amount. Please try again.\n")
+#             continue
+#         else:
+#             if int(withdraw_amount) <= balance:
+#                 print(f"\nYou have withdrawn ${withdraw_amount}.\n")
+#                 balance -= int(withdraw_amount)
+#             else:
+#                 print(f"\nYou cannot withdraw more than ${balance}.\nPlease try again.\n")
+#                 continue
+#     elif choice == "2":
+#         deposit_amount = input("Enter deposit amount: ")
+#         if not deposit_amount.isdigit():     
+#             print("\nInvalid amount. Please try again.\n")
+#             continue
+#         else:
+#             balance += int(deposit_amount)
+#             print(f"\nYou have deposited ${deposit_amount}.\n")
+#     elif choice == "3":
+#         print(f"\nCurrent balance: ${balance}\n")
+#     elif choice == "4":
+#         print("\nThank you for banking at this ATM.\nHave a nice day!")
+#         break
+#     else:
+#         print("Invalid choice. Please try again.")
+
+# Task 1
+groceries = [
+    "Bread",
+    "Milk",
+    "Eggs",
+    "Potato Chips",
+    "Fruits",
+    "Ice Cream",
+    "Tea",
+    "Coffee"
+]
+
+groceries[7] = "Banana"
+# print(groceries)
+
+# for item in groceries:
+#     print(item)
+
+# for i in range(len(groceries)):
+#     print(groceries[i])
+
+# Add at a certain index
+groceries.insert(1, "Apple")
+print(groceries)
+
+# Add to the end
+groceries.append("Peanuts")
+print(groceries)
+
+del(groceries[0])
+removed = groceries.pop(5)
+print(groceries)
+print(removed)
